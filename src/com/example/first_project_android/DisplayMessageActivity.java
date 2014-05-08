3a6a910ce982e6e7d026c70b5e1ba87d9eae86cb
Alessandro Stein <alessandro@alessandrostein.com>
2014-05-08 12:00:18 -0300

    Starting Another Activity

--- src/com/example/first_project_android/DisplayMessageActivity.java
+++ src/com/example/first_project_android/DisplayMessageActivity.java
@@ -9,28 +9,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
-	
-	public final static String EXTRA_MESSAGE =	"com.example.myfirstapp.MESSAGE";
-	
+public class DisplayMessageActivity extends ActionBarActivity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		Intent intent = getIntent();
+		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		
+		// Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(40);
+	    textView.setText(message);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+	    // Set the text view as the activity layout
+	    setContentView(textView);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.display_message, menu);
 		return true;
 	}
 
@@ -57,24 +59,10 @@ public class MainActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+			View rootView = inflater.inflate(R.layout.fragment_display_message,
+					container, false);
 			return rootView;
 		}
-	}
-	
-	/** Por Alessandro Stein
-	 *  Reference: https://developer.android.com/training/basics/firstapp/starting-activity.html
-	 *  Called when the user clicks the Send button
-	 */
-	public void sendMessage(View view){
-		// Do something in response to button
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-		startActivity(intent);
-		
 	}
 
 }
